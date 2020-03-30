456331bf1e5f585d9fed759874346e2b4d0d72a9
ivanzabelin <motoface@yandex.ru>
2020-03-30 15:16:00 +0300

    add

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -1,21 +1,38 @@
 package storage
 
 import (
-	"fmt"
 	"sync"
-	)
+)
 
-type Storage struct {
-	Data int
+type DataCache struct {
+	cache map[string]string
+	mu sync.RWMutex
 }
 
-func (c *Storage) Set(number int, ch chan bool, mutex *sync.Mutex) {
-	mutex.Lock()    // блокируем доступ к переменной counter
-	c.Data = 0
-	for k := 1; k <= 5; k++{
-		c.Data++
-		fmt.Println("Goroutine", number, "-", c.Data)
+func (c *DataCache) Set(key string,  value string) {
+	c.cache = make(map[string]string)
+	c.mu.Lock()
+	c.cache[key] = value
+	c.mu.Unlock()
+}
+
+func (c *DataCache) Get(key string) string {
+	c.mu.Lock()
+	name := c.cache[key]
+	defer c.mu.Unlock()
+	if name == "" {
+		return "empty value"
 	}
-	mutex.Unlock()  // деблокируем доступ
-	ch <- true
+	return name
+}
+
+func (c *DataCache) Delete(key string) string {
+	c.mu.Lock()
+	_, ok := c.cache[key];
+	if ok {
+		delete(c.cache, key);
+		return "key deleted"
+	}
+	defer c.mu.Unlock()
+	return "key not find"
 }
